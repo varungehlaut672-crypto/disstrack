e10cf7499840b58eab23804b3a2974d9735d65de
varungehlaut672-crypto <varungehlaut672@gmail.com>
2026-04-24 01:46:59 +0530

    made changes to 2 files

--- frontend/script.js
+++ frontend/script.js
@@ -1,30 +1,17 @@
-// Create map
 var map = L.map('map').setView([20, 0], 2);
 
-// Add map tiles
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap'
-}).addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+.addTo(map);
 
-// When user clicks anywhere
-map.on('click', function(e) {
+map.on('click', function () {
 
-    console.log("Clicked at:", e.latlng);
-
-    // Call backend API
     fetch("http://127.0.0.1:5000/risk/india")
-    .then(response => response.json())
+    .then(res => res.json())
     .then(data => {
-
-        // Show result
-        alert(
-            "Country: " + data.country +
-            "\nFlood Risk: " + data.flood_risk +
-            "\nEarthquake Risk: " + data.earthquake_risk
-        );
+        alert(`Country: ${data.country}\nRisk: ${data.risk}`);
     })
-    .catch(error => {
-        console.error("Error:", error);
+    .catch(err => {
+        alert("Backend not connected yet!");
     });
 
 });
