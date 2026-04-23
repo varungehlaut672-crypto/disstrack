6493d08faccd1e2f2ab6eb0fc055108a062e22ed
varungehlaut672-crypto <varungehlaut672@gmail.com>
2026-04-24 01:44:08 +0530

    made a new file

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,30 @@
+// Create map
+var map = L.map('map').setView([20, 0], 2);
 
+// Add map tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+// When user clicks anywhere
+map.on('click', function(e) {
+
+    console.log("Clicked at:", e.latlng);
+
+    // Call backend API
+    fetch("http://127.0.0.1:5000/risk/india")
+    .then(response => response.json())
+    .then(data => {
+
+        // Show result
+        alert(
+            "Country: " + data.country +
+            "\nFlood Risk: " + data.flood_risk +
+            "\nEarthquake Risk: " + data.earthquake_risk
+        );
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
+
+});
